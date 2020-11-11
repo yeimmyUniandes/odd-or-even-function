9313b85a51b5f0855f4dbe8502a613b26b07bf92
yeimmyUniandes <73206800+yeimmyUniandes@users.noreply.github.com>
2020-11-11 15:46:25 -0500

    Pruebas unitarias

--- src/main/java/com/microsoft/azure/Function.java
+++ src/main/java/com/microsoft/azure/Function.java
@@ -16,7 +16,7 @@ public class Function {
         String numberQueryValue = request.getQueryParameters().get("number");
         try {
             int number = Integer.parseInt(numberQueryValue);
-            String response = String.format("The number %d is %s.", number, number % 2 == 0 ? "Even" : "Odd");
+            String response = String.format("El número %d es %s.", number, number % 2 == 0 ? "Par" : "Impar");
 
             return request.createResponseBuilder(HttpStatus.OK).body(response).build();
         } catch (NumberFormatException nfe) {
